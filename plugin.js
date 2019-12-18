51b968a0fa4d791c5c2111142c4983076af3d726
egor <codeegor@gmail.com>
2019-12-18 23:44:38 +0200

    fix bug

--- plugin.js
+++ plugin.js
@@ -2,23 +2,23 @@ export default {
   install($Vue){
     $Vue.prototype.$debug = $Vue.config.devtools;
     $Vue.prototype.$cloneObj = (obj) => {return JSON.parse(JSON.stringify(obj));};
-    $Vue.prototype.$openPDFb64 = function(data, name){
-      if(this.$debug) console.log('show pdf', name);
+    $Vue.prototype.$openPDFb64 = (data, name) => {
+      // if(this.$debug) console.log('show pdf', name);
       let a = document.createElement("a");
       a.href = "data:application/pdf;base64,"+data;
       a.download = name;
       a.target = "_blank";
       a.click();
     };
-    $Vue.prototype.$openFile = function(url, name){
-      if(this.$debug) console.log('show file', name);
+    $Vue.prototype.$openFile = (url, name) => {
+      // if(this.$debug) console.log('show file', name);
       let a = document.createElement("a");
       a.href = url;
       // a.download = name;
       a.target = "_blank";
       a.click();
     };
-    $Vue.prototype.$imgSrcB64 = (f){
+    $Vue.prototype.$imgSrcB64 = f => {
       return 'data:'+f.mime+';base64,'+f.data;
     }
   }
